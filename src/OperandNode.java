bb46199c53fe82f03b86da267ad408c6f844868b
Lukas <lk.schrozberg@gmail.com>
2020-02-07 10:54:36 +0100

    Initial commit

---
+++ src/OperandNode.java
@@ -0,0 +1,13 @@
+public class OperandNode extends SyntaxNode implements Visitable {
+    public int position;
+    public String symbol;
+
+    public OperandNode(String symbol){
+        position = -1;      //bedeutet: noch nicht initialisiert
+        this. symbol = symbol;
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
---
+++ src/UnaryOpNode.java
@@ -0,0 +1,13 @@
+public class UnaryOpNode extends SyntaxNode implements Visitable {
+    public String operator;
+    public Visitable subNode;
+
+    public UnaryOpNode(String operator, Visitable subNode){
+        this.operator = operator;
+        this.subNode=subNode;
+    }
+
+    public void accept(Visitor visitor){
+        visitor.visit(this);
+    }
+}
